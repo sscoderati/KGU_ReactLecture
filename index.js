f7079aa48ee2a60bb71e62cf0c1d7684d7c109f3
sscoderati <chungup5495@gmail.com>
2021-09-13 16:31:49 +0900

    HomeWork Upgrade

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const changeWord = document.getElementById("changed-word");
 const targetWord = document.getElementById("target-word");
 const submit = document.getElementById("submit");
 const result = document.getElementById("new-text");
+const clear = document.getElementById("clear");
+const clipboard = document.getElementById("clipboard");
 
-function changeTextFunc(text, word, target){
+function changeTextFunc(text, target, word){
 
-	const changedText = text.replaceAll(word, target);
+	const changedText = text.replaceAll(target, word);
 	return changedText
 	
 }
@@ -17,7 +19,17 @@ const onClickSubmit = (e) => {
 	const word = changeWord.value
 	const target = targetWord.value
 	
-	result.value = changeTextFunc(text, word, target)
+	result.value = changeTextFunc(text, target, word)
+}
+const onClickClear = (e) => {
+	textArea.value = ""
+	result.value = ""
+}
+const onClickClipboard = (e) => {
+	result.select();
+	document.execCommand('copy');
 }
 
 submit.addEventListener('click', onClickSubmit);
+clear.addEventListener('click', onClickClear);
+clipboard.addEventListener('click', onClickClipboard);
